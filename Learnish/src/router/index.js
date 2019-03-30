e6f8421d4d6aa81b1936543fe209c9176bb3721e
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2019-03-30 12:15:53 +0100

    Router for login logic, fake-backend

--- Learnish/src/router/index.js
+++ Learnish/src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import LoginPage from '@/components/LoginPage';
+import RegisterPage from '@/components/RegisterPage';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello,
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: RegisterPage,
+    },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+  console.log('test');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  return next();
+});
+
+export default router;
