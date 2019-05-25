f7734d23ce7b99e390104de70efca6a2aa17f916
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2019-05-25 17:23:16 +0200

    Add WordsBag button

--- src/words/WordsBagsPage.vue
+++ src/words/WordsBagsPage.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <div class="display-3">Your wordsbags:</div>
+        <div>
+            <div class="display-3">Your wordsbags:</div>
+            <router-link class="btn btn-outline-success m-2" :to="'/word-bag/addWordBag'">
+            Add a new wordsBag
+            </router-link>
+        </div>
         <div v-if="wordsBags.items">
             <div class="list-group" v-for="wordsBag in wordsBags.items" :key="wordsBag.pk">
                 <router-link class="word list-group-item p-3 pl-5 m-2 btn btn-outline-secondary btn-lg" :to="'/words/' + wordsBag.pk">
